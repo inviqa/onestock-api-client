d40d7939d9b455fb725e970a706ee873047bac69
Géza Búza <gbuza@inviqa.com>
2020-02-11 18:11:56 +0100

    Create a custom normalizer to remove empty attributes

--- src/Inviqa/OneStock/Factory/SerializerFactory.php
+++ src/Inviqa/OneStock/Factory/SerializerFactory.php
@@ -2,26 +2,14 @@
 
 namespace Inviqa\OneStock\Factory;
 
+use Inviqa\OneStock\Normalizer\SkipEmptyPropertyNormalizer;
 use Symfony\Component\Serializer\Encoder\JsonEncoder;
-use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
-use Symfony\Component\Serializer\Normalizer\AbstractObjectNormalizer;
-use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 use Symfony\Component\Serializer\Serializer;
 
 class SerializerFactory
 {
     public function createSerializer(): Serializer
     {
-        $arrayCallback = function ($innerObject) {
-            return is_array($innerObject) && empty($innerObject) ? null : $innerObject;
-        };
-
-        return new Serializer([new ObjectNormalizer(null, null, null, null, null, null, [
-            AbstractObjectNormalizer::SKIP_NULL_VALUES => true,
-            AbstractNormalizer::CALLBACKS => [
-                'information' => $arrayCallback,
-                'types' => $arrayCallback,
-            ],
-        ])], [new JsonEncoder()]);
+        return new Serializer([new SkipEmptyPropertyNormalizer()], [new JsonEncoder()]);
     }
 }
